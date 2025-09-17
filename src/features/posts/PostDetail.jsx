69dc6a7053b6a14306e61b77e05d3f6747559882
Earl Lauriece S. Butlay <earl0bluesky142226@gmail.com>
2025-09-17 10:54:48 +0800

    @stage 3, modified to make it mobile responsive

--- src/features/posts/PostDetail.jsx
+++ src/features/posts/PostDetail.jsx
@@ -1,5 +1,6 @@
 import { useParams, useNavigate } from "react-router-dom";
 import { useSelector } from "react-redux";
+import './PostDetail.css';
 
 function PostDetail() {
   const { postId } = useParams();
