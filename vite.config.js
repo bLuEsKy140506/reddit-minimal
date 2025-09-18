49661aba342be503791ed4d476a1e7748a302051
Earl Lauriece S. Butlay <earl0bluesky142226@gmail.com>
2025-09-18 13:30:53 +0800

    fixing data fetching reddit to web hosting take 12

--- vite.config.js
+++ vite.config.js
@@ -5,14 +5,7 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   base: "/", // ✅ ensures assets resolve properly in Vercel
-  server: {
-    proxy: {
-      "/api": {
-        target: "https://www.reddit.com",
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ""), // remove `/api`
-      },
-    },
-  },
+  
+  
 });
 
